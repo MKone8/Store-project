392d7829a5a662a8e8ff090db6927bcaaf024947
Michal <mkawalec181b@gmail.com>
2022-10-26 18:36:54 +0200

    Stworzenie plików

---
+++ src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        System.out.println("Witaj w naszym sklepie!");
+        System.out.println("----------------------");
+        System.out.println("1. Zaloguj się!");
+        System.out.println("2. Kontynuuj jako gość");
+        System.out.println("9. Wyjdź");
+        
+
+    }
+}
