187cd2fede73337dae7ff11be288455cac919fd0
Michal <mkawalec181b@gmail.com>
2022-10-26 19:12:39 +0200

    Admin -> Logowanie...

--- src/App.java
+++ src/App.java
@@ -1,13 +1,10 @@
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
-        System.out.println("Witaj w naszym sklepie!");
-        System.out.println("----------------------");
-        System.out.println("1. Zaloguj się!");
-        System.out.println("2. Kontynuuj jako gość");
-        System.out.println("9. Wyjdź");
         
-
+        Menu show = new Menu();
+        show.display();
     }
 }
--- src/Menu.java
+++ src/Menu.java
@@ -1,3 +1,29 @@
+import java.util.Scanner;
+
 public class Menu {
-    
-}
+    public void display (){
+
+    System.out.println("Witaj w naszym sklepie!");
+    System.out.println("----------------------");
+    System.out.println("1. Zaloguj się!");
+    System.out.println("2. Kontynuuj jako gość");
+    System.out.println("9. Wyjdź");
+    Scanner sc = new Scanner(System.in);
+    int menu_nr = sc.nextInt();
+    System.out.println("Wybrana liczba to:"+menu_nr);
+    switch(menu_nr){
+        case 1:
+        
+        System.out.println("Podaj login: ");
+        String login = sc.next();        
+        System.out.println("Podaj hasło: "+sc.nextLine());
+        String password = sc.next();
+        break;
+        case 2:
+        System.out.println("Przejdzie do opcji wyszukiwania");
+        break;
+        case 9:
+        System.out.println("Ciao bella!");
+        sc.close();
+    }
+}}
